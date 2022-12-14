5565b56e1903788f4d860c3cba6cf7c956248fea
natadekoko <60341975+natadekoko-rin@users.noreply.github.com>
2022-12-14 08:05:34 +0700

    tambah logic 1 OOP

---
+++ java-oop2/src/logic/Logic01_06.java
@@ -0,0 +1,19 @@
+package logic;
+
+public class Logic01_06 extends BasicLogic{
+    public Logic01_06(int n) {
+        super(n);
+    }
+
+    public void isiArray(){
+//        for (int i = 0; i < this.n; i++) {
+//            System.out.print("trouble");
+//        }
+//        System.out.println();
+    }
+    public void cetakArray(){
+//        this.isiArray();
+//        this.printSingle();
+        System.out.println("trouble");
+    }
+}
