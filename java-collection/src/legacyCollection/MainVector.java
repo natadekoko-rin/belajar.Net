1896535eee3c34fa1121f59386cfe0e43939d7c9
natadekoko <60341975+natadekoko-rin@users.noreply.github.com>
2022-12-19 06:58:22 +0700

    Stack + Hash Table + Vector

--- java-collection/src/legacyCollection/MainVector.java
+++ java-collection/src/legacyCollection/MainVector.java
@@ -7,7 +7,14 @@ import java.util.Vector;
 
 public class MainVector {
     public static void main(String[] args) {
-        List<String, Transport> factory = new Vector<>();
-        factory.add("PT. Suka-suka", new Transport(8,"Private","Rose", 6));
+        List<String> factory = new Vector<>();
+//        factory.add("PT. Suka-suka", new Transport(8,"Private","Rose", 6));
+        factory.add("PT. Suka-suka");
+        factory.add("PT.Cuma pt");
+        System.out.println(factory);
+
+        for (var pabrik : factory){
+            System.out.println(pabrik);
+        }
     }
 }
